b72b9dc73449514eb17737ab42edb4d45adacf3a
Sundhar Vinodh Sangeetha <sundharvs@gmail.com>
2023-04-13 14:01:20 -0400

    Working initial commit

---
+++ main/local_control.c
@@ -0,0 +1,256 @@
+/*
+ * SPDX-FileCopyrightText: 2022-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <inttypes.h>
+#include "esp_log.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/timers.h"
+
+#include "esp_mac.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+
+#include <sys/socket.h>
+
+#include "esp_bridge.h"
+#include "esp_mesh_lite.h"
+#include "lwip/lwip_napt.h"
+
+#define PAYLOAD_LEN       (1456) /**< Max payload size(in bytes) */
+#define IP_PROTO_TCP     6
+#define IP_PROTO_UDP     17
+
+static int g_sockfd    = -1;
+static const char *TAG = "local_control";
+
+uint32_t sta_addr;
+
+/**
+ * @brief Create a tcp client
+ */
+static int socket_tcp_client_create(const char *ip, uint16_t port)
+{
+    ESP_LOGI(TAG, "Create a tcp client, ip: %s, port: %d", ip, port);
+
+    esp_err_t ret = ESP_OK;
+    int sockfd    = -1;
+    struct ifreq iface;
+    memset(&iface, 0x0, sizeof(iface));
+    struct sockaddr_in server_addr = {
+        .sin_family = AF_INET,
+        .sin_port = htons(port),
+        .sin_addr.s_addr = inet_addr(ip),
+    };
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        ESP_LOGE(TAG, "socket create, sockfd: %d", sockfd);
+        goto ERR_EXIT;
+    } 
+
+    esp_netif_get_netif_impl_name(esp_netif_get_handle_from_ifkey("WIFI_STA_DEF"), iface.ifr_name);
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BINDTODEVICE,  &iface, sizeof(struct ifreq)) != 0) {
+        ESP_LOGI(TAG, "Bind [sock=%d] to interface %s fail", sockfd, iface.ifr_name);
+    }
+
+    ret = connect(sockfd, (struct sockaddr *)&server_addr, sizeof(struct sockaddr_in));
+    if (ret < 0) {
+        ESP_LOGE(TAG, "socket connect, ret: %d, ip: %s, port: %d",
+                   ret, ip, port);
+        goto ERR_EXIT;
+    }
+    return sockfd;
+
+ERR_EXIT:
+
+    if (sockfd != -1) {
+        close(sockfd);
+    }
+
+    return -1;
+}
+
+void tcp_client_write_task(void *arg)
+{
+    size_t size        = 0;
+    int count          = 0;
+    char *data         = NULL;
+    esp_err_t ret      = ESP_OK;
+    uint8_t sta_mac[6] = {0};
+
+    esp_wifi_get_mac(ESP_IF_WIFI_STA, sta_mac);
+
+    ESP_LOGI(TAG, "TCP client write task is running");
+
+    while (1) {
+        if (g_sockfd == -1) {
+            vTaskDelay(500 / portTICK_PERIOD_MS);
+            continue;
+        }
+
+        vTaskDelay(3000 / portTICK_PERIOD_MS);
+
+        size = asprintf(&data, "{\"src_addr\": \"" MACSTR "\",\"data\": \"Hello TCP Server!\",\"count\": %d}",
+                        MAC2STR(sta_mac), count++);
+
+        ESP_LOGD(TAG, "TCP write, size: %d, data: %s", size, data);
+        ret = write(g_sockfd, data, size);
+        free(data);
+
+        if (ret <= 0) {
+            ESP_LOGE(TAG, "<%s> TCP write", strerror(errno));
+            continue;
+        }
+    }
+
+    ESP_LOGI(TAG, "TCP client write task is exit");
+
+    close(g_sockfd);
+    g_sockfd = -1;
+    free(data);
+    vTaskDelete(NULL);
+}
+
+/**
+ * @brief Timed printing system information
+ */
+static void print_system_info_timercb(TimerHandle_t timer)
+{
+    uint8_t primary                 = 0;
+    uint8_t sta_mac[6]              = {0};
+    wifi_ap_record_t ap_info        = {0};
+    wifi_second_chan_t second       = 0;
+    wifi_sta_list_t wifi_sta_list   = {0x0};
+
+    esp_wifi_sta_get_ap_info(&ap_info);
+    esp_wifi_get_mac(ESP_IF_WIFI_STA, sta_mac);
+    esp_wifi_ap_get_sta_list(&wifi_sta_list);
+    esp_wifi_get_channel(&primary, &second);
+
+    ESP_LOGI(TAG, "System information, channel: %d, layer: %d, self mac: " MACSTR ", parent bssid: " MACSTR
+             ", parent rssi: %d, free heap: %"PRIu32"", primary,
+             esp_mesh_lite_get_level(), MAC2STR(sta_mac), MAC2STR(ap_info.bssid),
+             (ap_info.rssi != 0 ? ap_info.rssi : -120), esp_get_free_heap_size());
+
+    for (int i = 0; i < wifi_sta_list.num; i++) {
+        ESP_LOGI(TAG, "Child mac: " MACSTR, MAC2STR(wifi_sta_list.sta[i].mac));
+    }
+}
+
+static void ip_event_sta_got_ip_handler(void *arg, esp_event_base_t event_base,
+                                        int32_t event_id, void *event_data)
+{
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *) event_data;
+    ESP_LOGI(TAG, "Connected with IP Address:" IPSTR, IP2STR(&event->ip_info.ip));
+
+    sta_addr = event->ip_info.ip.addr;
+
+    //ip_portmap_add(IP_PROTO_TCP, event->ip_info.ip.addr, 8554, ESP_IP4TOADDR(192, 168, 4, 2), 8554);
+    //ip_portmap_add(IP_PROTO_UDP, event->ip_info.ip.addr, 8554, ESP_IP4TOADDR(192, 168, 4, 2), 8554);
+    //if (g_sockfd != -1) {
+    //    close(g_sockfd);
+    //    g_sockfd = -1;
+    //}
+
+    //g_sockfd = socket_tcp_client_create(CONFIG_SERVER_IP, CONFIG_SERVER_PORT);
+
+    //static bool tcp_task = false;
+
+    //if (!tcp_task) {
+    //    xTaskCreate(tcp_client_write_task, "tcp_client_write_task", 4 * 1024,
+    //                NULL, 5, NULL);
+    //    tcp_task = true;
+    //}
+}
+
+static void ip_event_ap_staipassigned_handler(void *arg, esp_event_base_t event_base,
+                                        int32_t event_id, void *event_data)
+{
+    ip_event_ap_staipassigned_t *event = (ip_event_ap_staipassigned_t *) event_data;
+    ESP_LOGI(TAG, "STA Connected to SoftAP with IP Address:" IPSTR, IP2STR(&event->ip));
+
+    ip_portmap_add(IP_PROTO_TCP, sta_addr, 8554, event->ip.addr, 8554); // RTSP TCP Port
+    ip_portmap_add(IP_PROTO_UDP, sta_addr, 8554, event->ip.addr, 8554); // RTSP UDP Port
+    ip_portmap_add(IP_PROTO_TCP, sta_addr, 22, event->ip.addr, 22); // SSH TCP Port
+    ip_portmap_add(IP_PROTO_UDP, sta_addr, 22, event->ip.addr, 22); // SSH UDP Port
+    ip_portmap_add(IP_PROTO_TCP, sta_addr, 14551, event->ip.addr, 14551); // QGroundControl TCP Port
+    ip_portmap_add(IP_PROTO_UDP, sta_addr, 14551, event->ip.addr, 14551); // QGroundControl UDP Port
+    ip_portmap_add(IP_PROTO_TCP, sta_addr, 11311, event->ip.addr, 11311); // ROS TCP Port
+    ip_portmap_add(IP_PROTO_UDP, sta_addr, 11311, event->ip.addr, 11311); // ROS TCP Port
+    ESP_LOGI(TAG, "Added portmap");
+}
+
+
+static esp_err_t esp_storage_init(void)
+{
+    esp_err_t ret = nvs_flash_init();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        // NVS partition was truncated and needs to be erased
+        // Retry nvs_flash_init
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    return ret;
+}
+
+static esp_err_t wifi_init(void)
+{
+    // Station
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_ROUTER_SSID,
+            .password = CONFIG_ROUTER_PASSWORD,
+        },
+    };
+    esp_bridge_wifi_set(WIFI_MODE_STA, (char *)wifi_config.sta.ssid, (char *)wifi_config.sta.password, NULL);
+
+    // Softap
+    memset(&wifi_config, 0x0, sizeof(wifi_config_t));
+    size_t softap_ssid_len = sizeof(wifi_config.ap.ssid);
+    if (esp_mesh_lite_get_softap_ssid_from_nvs((char *)wifi_config.ap.ssid, &softap_ssid_len) != ESP_OK) {
+        snprintf((char *)wifi_config.ap.ssid, sizeof(wifi_config.ap.ssid), "%s", CONFIG_BRIDGE_SOFTAP_SSID);
+    }
+    size_t softap_psw_len = sizeof(wifi_config.ap.password);
+    if (esp_mesh_lite_get_softap_psw_from_nvs((char *)wifi_config.ap.password, &softap_psw_len) != ESP_OK) {
+        strlcpy((char *)wifi_config.ap.password, CONFIG_BRIDGE_SOFTAP_PASSWORD, sizeof(wifi_config.ap.password));
+    }
+    esp_bridge_wifi_set(WIFI_MODE_AP, (char *)wifi_config.ap.ssid, (char *)wifi_config.ap.password, NULL);
+
+    return ESP_OK;
+}
+
+void app_main()
+{
+    /**
+     * @brief Set the log level for serial port printing.
+     */
+    esp_log_level_set("*", ESP_LOG_INFO);
+
+    esp_storage_init();
+
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    esp_bridge_create_all_netif();
+
+    wifi_init();
+
+    esp_mesh_lite_config_t mesh_lite_config = ESP_MESH_LITE_DEFAULT_INIT();
+    esp_mesh_lite_init(&mesh_lite_config);
+
+    /**
+     * @breif Create handler
+     */
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &ip_event_sta_got_ip_handler, NULL, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT, IP_EVENT_AP_STAIPASSIGNED, &ip_event_ap_staipassigned_handler, NULL, NULL));
+
+    TimerHandle_t timer = xTimerCreate("print_system_info", 10000 / portTICK_PERIOD_MS,
+                                       true, NULL, print_system_info_timercb);
+    xTimerStart(timer, 0);
+}
